fc1dd930fd26fdbffed798cb3af3feb823e6fbcc
Seth Christopher <seth.christopher93@gmail.com>
2025-03-13 23:15:18 -0400

    stateful variables, etc.

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
 #app.py
+import pandas as pd
 import scipy.stats
 import streamlit as st
 import time
-import pandas as pd
+
+# these are stateful variables which are preserved as Streamlit reruns this script
+if 'experiment_no' not in st.session_state:
+    st.session_state['experiment_no'] = 0
+
+if 'df_experiment_results' not in st.session_state:
+    st.session_state['df_experiment_results'] = pd.DataFrame(columns=['no', 'iterations', 'mean'])
 
 st.header('Tossing a Coin')
 
-data = pd.DataFrame({'value': [0.5]})
-chart = st.line_chart(data)
+chart = st.line_chart([0.5])
 
 def toss_coin(n):
 
@@ -31,4 +37,18 @@ number_of_trials = st.slider('Number of trials?', 1, 1000, 10)
 start_button = st.button('Run')
 
 if start_button:
-    st.write(f'Running the experient of {number_of_trials} trials.')
+    st.write(f'Running the experiment of {number_of_trials} trials.')
+    st.session_state['experiment_no'] += 1
+    mean = toss_coin(number_of_trials)
+    st.session_state['df_experiment_results'] = pd.concat([
+        st.session_state['df_experiment_results'],
+        pd.DataFrame(data=[[st.session_state['experiment_no'],
+                            number_of_trials,
+                            mean]],
+                     columns=['no', 'iterations', 'mean'])
+        ],
+        axis=0)
+    st.session_state['df_experiment_results'] = \
+        st.session_state['df_experiment_results'].reset_index(drop=True)
+
+st.write(st.session_state['df_experiment_results'])
